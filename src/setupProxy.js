64b8b16171c355e415dd50ffa4f5df5a277e2f91
Boris Civcic <civcicboris93@gmail.com>
2019-12-29 16:54:57 +0100

    setupproxy fixed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy(["/public"], {
-      target: "https://api-test.services.distll.com"
+      target: "https://api-test.services.distll.com",
+      changeOrigin: false
     })
   );
 };
