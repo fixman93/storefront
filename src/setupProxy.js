1dc0b6501364d9a6601a58797830b7f9c62da163
Boris Civcic <civcicboris93@gmail.com>
2019-12-30 18:42:04 +0100

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
-// const proxy = require("http-proxy-middleware");
+const proxy = require("http-proxy-middleware");
 
-// module.exports = function(app) {
-//   app.use(
-//     proxy(["/public"], {
-//       target: "https://api-test.services.distll.com",
-//       changeOrigin: false
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    proxy(["/public"], {
+      target: "https://api-test.services.distll.com",
+      changeOrigin: false
+    })
+  );
+};
 
 // app.use (
 //   proxy(['/public', '/cart'], {target: "https://api-test.services.distll.com",
